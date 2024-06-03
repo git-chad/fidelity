3549f77dbabed657b429e6e48329841360e9d105
git-chad <tobimocc@gmail.com>
2024-06-03 18:25:58 -0300

    fix(dev): let's see if this works now

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,19 @@
-import Head from "next/head";
-import type { Metadata } from "next";
-import { GeistSans } from "geist/font/sans";
-import { ViewTransitions } from "next-view-transitions";
-import "./globals.css";
-import Navbar from "@/components/navigation/navbar";
+import type { Metadata } from 'next';
+import { GeistSans } from 'geist/font/sans';
+import { ViewTransitions } from 'next-view-transitions';
+import './globals.css';
+import Navbar from '@/components/navigation/navbar';
 
 export const metadata: Metadata = {
-  title: { default: "Fidelity", template: "%s - Fidelity" },
-  description: "Holy trinity of fidelity",
+  title: { default: 'Fidelity', template: '%s - Fidelity' },
+  description: 'Holy trinity of fidelity',
+  openGraph: {
+    images: '/opengraph-image.jpg',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    images: '/twitter-image.jpg',
+  },
 };
 
 export default function RootLayout({
@@ -15,24 +21,10 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const baseUrl = "https://fidelity-orcin.vercel.app";
-
   return (
     <ViewTransitions>
-      <html className={GeistSans.className} lang="en">
-        <Head>
-          <title>Fidelity</title>
-          <meta name="description" content="Holy trinity of fidelity" />
-          <meta property="og:title" content="Fidelity" />
-          <meta property="og:description" content="Holy trinity of fidelity" />
-          <meta property="og:url" content={baseUrl} />
-          <meta property="og:image" content={`${baseUrl}/opengraph-image.jpg`} />
-          <meta property="og:image:alt" content="Happy Coding" />
-          <meta property="og:type" content="website" />
-          <meta property="og:site_name" content="Fidelity" />
-          <meta name="twitter:image" content={`${baseUrl}/twitter-image.jpg`} />
-          <meta name="twitter:image:alt" content="Happy Coding" />
-        </Head>
+      <html className={GeistSans.className} lang='en'>
+        <head />
         <body>
           <Navbar />
           {children}
