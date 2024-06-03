52bc441a1a83d1ebe6b2c8c2bfb62658fb6ecd1c
git-chad <tobimocc@gmail.com>
2024-06-03 17:51:06 -0300

    chore(assets): add og image

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <main className="bg-pri-black">
       <Container className="min-h-svh flex flex-col justify-center">
       <h1 className="text-h1 leading-none tracking-tighter font-semibold">Fidelity</h1>
+      <img src="/images/og-img.jpg"/>
       </Container>
     </main>
   );
